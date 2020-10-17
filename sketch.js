a24eb394761435bab6a10005f5cc496e0abb398e
Ayaan-Rana <71192735+Ayaan-Rana@users.noreply.github.com>
2020-10-17 15:05:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var paperOBJ;
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paperOBJ = new Paper(100, 100, 10);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+	paperOBJ.display(); 
+}
